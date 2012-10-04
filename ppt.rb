a40dbd448d461f225562744368f8d7ee9b35dab0
Fernando González López-Peñalver <alu0100256543@ull.edu.es>
2012-10-04 21:47:53 +0100

    Eliminadas las funciones, ahora es mucho 'más imperativo'

--- ppt.rb
+++ ppt.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+# encoding: utf-8
 
 #
 #	Fernando González López-Peñalver (alu3177)
@@ -8,56 +9,51 @@
 #					Versión implementada con las sentencias de control CASE y WHEN
 #
 
-EMPATE = "      Empate"
-WIN = "      ¡Has Ganado!"
-LOOSE = "      Otra vez será"
+EMPATE = "                            Empate..."
+WIN = "                         Has Ganado!"
+LOOSE = "                        Otra vez será"
 $machine = `hostname`.strip # Obtenemos el nombre de la máquina (strip elimina \n)
 
-def PiedraPapelTijeras(jugador, oponente)
-    puts "        ## JUGADOR ##                         ## #{$machine} ##"
-    puts "           #{jugador.to_s.capitalize}               VS               #{oponente.to_s.capitalize}"
-    case jugador
-        when :piedra
-            case oponente
-                when :piedra
-                    puts EMPATE
-                when :papel
-                    puts LOOSE
-                when :tijeras
-                    puts WIN
-            end
-        when :papel
-            case oponente
-                when :piedra
-                    puts WIN
-                when :papel
-                    puts EMPATE
-                when :tijeras
-                    puts LOOSE
-            end
-        when :tijeras
-            case oponente
-                when :piedra
-                    puts LOOSE
-                when :papel
-                    puts WIN
-                when :tijeras
-                    puts EMPATE
-            end
-    end
+jugadas = [:piedra, :papel, :tijeras]
+
+if (ARGV.count == 1)and(jugadas.include?(ARGV[0].downcase.to_sym))
+	player = ARGV[0].to_sym
+else
+	raise ArgumentError, "Debe proporcionar un parametro de los siguientes: piedra, papel o tijeras"
 end
 
-def JugadaMaquina
-    random = rand(599)
-    case random
-        when 0..199
-            return :piedra
-        when 200..399
-            return :papel
-        when 400..599
-            return :tijeras
-    end
+oponente = jugadas.sample; # Generamos una jugada de la máquina
+puts "        ## JUGADOR ##                         ## #{$machine} ##"
+puts "           #{player.to_s.capitalize}               VS               #{oponente.to_s.capitalize}"
+puts ""
+case player
+    when :piedra
+        case oponente
+            when :piedra
+                puts EMPATE
+            when :papel
+                puts LOOSE
+            when :tijeras
+                puts WIN
+        end
+    when :papel
+        case oponente
+            when :piedra
+                puts WIN
+            when :papel
+                puts EMPATE
+            when :tijeras
+                puts LOOSE
+        end
+    when :tijeras
+        case oponente
+            when :piedra
+                puts LOOSE
+            when :papel
+                puts WIN
+            when :tijeras
+                puts EMPATE
+        end
 end
 
-PiedraPapelTijeras(:tijeras,JugadaMaquina())
 
